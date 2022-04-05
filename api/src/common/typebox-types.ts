d059b77f6d54a727885bb1a1caaf660c299dfcf0
Victor Wolmeister <wolmeistervictor@gmail.com>
2022-04-04 21:14:50 -0300

    Fix typebox date type format

--- api/src/common/typebox-types.ts
+++ api/src/common/typebox-types.ts
@@ -3,9 +3,9 @@ import { TSchema } from '@sinclair/typebox';
 export interface TDateType extends TSchema {
   $static: Date | string; // allow both Date and string
   type: 'string';
-  format: 'date';
+  format: 'date-time';
 }
 
 export function DateType(): TDateType {
-  return { type: 'string', format: 'date' } as never;
+  return { type: 'string', format: 'date-time' } as never;
 }

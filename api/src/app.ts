cc3d8b5fa57abb5785c11881e5f69d4b2b8b95e5
Victor Wolmeister <wolmeistervictor@gmail.com>
2022-04-06 20:11:48 -0300

    Update to openapi 3 and add auth

--- api/src/app.ts
+++ api/src/app.ts
@@ -6,11 +6,25 @@ import { userRoutes } from './modules/user';
 const app = fastify();
 
 app.register(swagger, {
-  swagger: {
+  openapi: {
     info: {
       title: 'Bigorna API',
       version: '0.0.1',
     },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
     tags: [{ name: 'Users', description: 'User related end-points' }],
   },
   routePrefix: '/docs',
